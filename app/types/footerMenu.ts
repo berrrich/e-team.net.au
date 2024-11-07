9d60e171dcd6f67baf5602902834a16d6a779827
berrrich <142289045+berrrich@users.noreply.github.com>
2024-11-07 21:52:13 +0800

    Eslint any bug

--- app/types/footerMenu.ts
+++ app/types/footerMenu.ts
@@ -2,6 +2,7 @@ export type FooterMenu = {
     title: string;
     navItems?: {
       label: string;
+      // eslint-disable-next-line  @typescript-eslint/no-explicit-any
       route: any;
     }[];
   };
--- app/types/menu.ts
+++ app/types/menu.ts
@@ -1,5 +1,6 @@
 export type Menu = {
     label: string;
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
     route?: any;
     children?: Menu[];
   };
