647be8269e45ab25551ae31f7f8b97ad1bd0e977
berrrich <142289045+berrrich@users.noreply.github.com>
2024-11-08 15:32:21 +0800

    More header

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -7,14 +7,14 @@ import Image from "next/image";
 const Header = () => {
 return (
 
-      <header className="navbar left-0 top-0 z-50 pt-8 px-4 w-full border-stroke bg-white dark:border-stroke-dark justify-center">
+      <header className="navbar left-0 top-0 z-50 pt-8 px-4 w-full border-stroke bg-white dark:border-stroke-dark">
         <div className="container relative max-w-[1330px]">
-            <div className="flex justify-between">
-            <div className="block pl-4 lg:py-0 w-3/8 sm-bg-">
+            <div className="flex flex-row justify-between items-center">
+            <div className="lg:py-0 w-3/8">
               <Link href="/" className="block">
                 <Image
-                  width={229}
-                  height={90}
+                  width={400}
+                  height={115}
                   src={"/images/logo/TSSHoldingLogo.png"}
                   alt="Logo"
                   priority
@@ -22,7 +22,8 @@ return (
                 />
               </Link>               
             </div>
-            <div className="container items-center w-5/8"><h1 className="text-black sm:text-[50px] md:text-[50px] lg:text-[42px] xl:text-[50px] font-bold italic text-center">ALL ELECTRIC LAWN SERVICE</h1></div></div></div>
+            <div className="w-5/8">
+            <h1 className="text-black sm:text-[50px] md:text-[50px] lg:text-[42px] xl:text-[50px] font-bold italic text-center">ALL ELECTRIC LAWN SERVICE</h1></div></div></div>
             </header>
         );
 };
