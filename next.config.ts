67410867de451949e8c53cf19d09bcb6377e1793
berrrich <142289045+berrrich@users.noreply.github.com>
2024-12-20 19:56:55 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  output: "export", // (optional) Set your desired output configuration
+  distDir:"build",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
