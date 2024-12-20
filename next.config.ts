13edec87482fe33d33dac1f71b34be0febb8aa3f
berrrich <142289045+berrrich@users.noreply.github.com>
2024-12-20 21:03:14 +0800

    DEbug service unavailable

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export", // (optional) Set your desired output configuration
-  distDir:"build",
-  images: {
-    unoptimized: true,
-  },
+  
 };
 
 export default nextConfig;
