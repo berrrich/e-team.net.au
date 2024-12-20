3303eda30ea1692b266091f110ece1ae8b39a003
berrrich <142289045+berrrich@users.noreply.github.com>
2024-12-20 19:41:26 +0800

    Service unavailable error

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // (optional) Set your desired output configuration
-  distDir:"build",
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
   
 };
 
